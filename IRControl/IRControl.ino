dfea843cc92d093a3f0cadf931c96e90b54a922b
Alex Fernandez <alex@xicre.com>
2018-05-13 10:42:50 -0600

    IRControl

--- IRControl/IRControl.ino
+++ IRControl/IRControl.ino
@@ -1,6 +1,6 @@
 #include <IRremote.h>
 
-int RECV_PIN = 11;
+int RECV_PIN = 2;
 
 IRrecv irrecv(RECV_PIN);
 
@@ -8,16 +8,14 @@ decode_results results;
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("Enabling IRin");
-  irrecv.enableIRIn(); // Start the receiver
-  Serial.println("Enabled IRin");
+  Serial.begin(9600); // Initiallize serial port at 9600 rate
+  irrecv.enableIRIn(); // Initiallize the receiver
 }
 
 void loop() {
   if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
+    Serial.println(results.value, HEX); // send the result to the serial port.
     irrecv.resume(); // Receive the next value
   }
-  delay(100);
+  delay(100); //Wait 100ms
 }
